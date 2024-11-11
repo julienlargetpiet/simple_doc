ad0e9aad1d11e5f91dc3df03af9c7d8af14982fd
Julien <pgyygp090@gmail.com>
2024-11-11 19:00:15 +0100

    created this repo

---
+++ README.md
@@ -0,0 +1,39 @@
+<img src = "logo.jpg">
+
+# simple_doc
+
+C++ programm to generate a html page for functions documentation.
+
+## Example
+
+Let's consider the file `code.py`:
+
+`
+##@T Add
+##@U Description paragraph1
+##@D Description paragraph2 <img src = "...">
+##@A x : first number to add
+##@A y : second number to add
+##@X
+##@E add(3, 4)
+##@E 7
+##@X
+
+add(a, b):
+    return a + b`
+
+## Usage:
+
+`g++ -03 cpp_doc.cpp`
+
+And then...
+
+`./a.out`
+
+`$File: code.py` 
+
+`$Path of the logo image if exists: logo.jpg`
+
+That's all.
+
+
---
+++ cpp_doc.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+  std::cout << "File: ";
+  std::string file;
+  std::cin >> file;
+  std::string logo;
+  std::cout << "Path of the logo image if exists: ";
+  std::cin >> logo;
+  std::fstream myfile(file);
+  std::fstream outfile("out.html", std::ios::out);
+  outfile << "<!DOCTYPE html>\n<head>\n<title>Documentation</title>\n</head>\n";
+  outfile << "<body>\n";
+  if (logo.length() > 0) {
+    outfile << "<img src =\"" << logo << "\" width=250 height=100>\n";
+  };
+  std::string currow;
+  std::string cur_doc;
+  int i;
+  int n;
+  bool D = 0;
+  bool A = 0;
+  bool E = 0;
+  while (getline(myfile, currow)) {
+    n = currow.length();
+    if (n > 3) {
+      if (currow[2] == '@') {
+        if (currow[3] == 'T') {
+          cur_doc = "<h1>";
+          for (i = 4; i < n; ++i) {
+            cur_doc += currow[i];
+          };
+          outfile << cur_doc << "</h1><br>\n";
+        } else if (currow[3] == 'U') {
+          cur_doc = "<h2>#Usage</h2><code>";
+          for (i = 4; i < n; ++i) {
+            cur_doc += currow[i];
+          };
+          outfile << cur_doc << "</code><br>\n";
+        } else if (currow[3] == 'D') {
+          if (!D) {
+            D = 1;
+            cur_doc = "<h2>#Description</h2><p>";
+          } else {
+            cur_doc = "<p>";
+          };
+          for (i = 4; i < n; ++i) {
+            cur_doc += currow[i];
+          };
+          outfile << cur_doc << "</p>\n";
+        } else if (currow[3] == 'A') {
+          if (!A) {
+            outfile << "<h2>#Arguments</h2><table>\n";
+            A = 1;
+          };
+          cur_doc = "<tr><th>";
+          for (i = 4; i < n; ++i) {
+            if (currow[i] == ':') {
+              cur_doc += "</th><th>";
+            } else {
+              cur_doc += currow[i];
+            };
+          };
+          outfile << cur_doc << "</th></tr>\n";
+        } else if (currow[3] == 'E') {
+          if (!E) {
+            cur_doc = "<h2>#Example(s)</h2><code>";
+            E = 1;
+          } else {
+            cur_doc = "";
+          };
+          for (i = 4; i < n; ++i) {
+            cur_doc += currow[i];
+          };
+          outfile << cur_doc << "<br>\n";
+        } else if (currow[3] == 'X') {
+          if (A) {
+            outfile << "</table><br>\n";
+            A = 0;
+          } else if (E) {
+            outfile << "</code><br>\n"; 
+            E = 0;
+          };
+        };
+      };
+    };
+  };
+  outfile << "</body>\n\n";
+  myfile.close();
+  outfile.close();
+  return 0;
+};
+
+
