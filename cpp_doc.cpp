756ab6d4c22581aa7db98cd81c2819cc6d50fb66
Julien <pgyygp090@gmail.com>
2024-11-17 22:55:00 +0100

    updated css and added customizable theme

--- cpp_doc.cpp
+++ cpp_doc.cpp
@@ -14,8 +14,123 @@ int main() {
   std::vector<std::string> mvec;
   std::cout << "Full path of the logo image if exists (\'N\' if it does not): ";
   std::cin >> logo;
-  std::fstream myfile(file);
+  std::string theme_file;
+  std::cout << "Full path of the theme file (\'D\' for default): ";
+  std::cin >> theme_file; 
+  std::string background_color = "#FFFFFF";
+  std::string top_color = "#2c4786";
+  std::string titles_color = "#2c4786";
+  std::string code_color = "#ac3a3a";
+  std::string font_color = "#000000";
+  std::string Title_color = "#FFFFFF";
+  std::string code_background_color = "#f1f1f1";
+  std::string toc_background_color = "#f1f1f1";
+  std::string doc_background_color = "#f1f1f1";
+  std::string font = "Times New Roman";
+  unsigned int n;
+  unsigned int i;
   std::string currow;
+  if (theme_file != "D") {
+   std::string cur_var;
+    std::fstream theme_File(theme_file);
+    std::string val_colfnt;
+    bool cur_val;
+    while (getline(theme_File, currow)) {
+      val_colfnt = "";
+      n = currow.length();
+      cur_val = 0;
+      i = 0;
+      cur_var = "";
+      if (currow.length() > 7) {
+        while (!cur_val) {
+          cur_var += currow[i];
+          if (cur_var == "background_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            background_color = val_colfnt;
+          } else if (cur_var == "top_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            top_color = val_colfnt;
+          } else if (cur_var == "titles_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            titles_color = val_colfnt;
+          } else if (cur_var == "code_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            code_color = val_colfnt;
+          } else if (cur_var == "font_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            font_color = val_colfnt;
+          } else if (cur_var == "Title_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            Title_color = val_colfnt;
+          } else if (cur_var == "font" & currow[i + 1] == ':') {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            font = val_colfnt;
+          } else if (cur_var == "code_background_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            code_background_color = val_colfnt;
+          } else if (cur_var == "toc_background_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            toc_background_color = val_colfnt;
+          } else if (cur_var == "doc_background_color") {
+            cur_val = 1;
+            i += 3;
+            while (i < n) {
+              val_colfnt += currow[i];
+              i += 1;
+            };
+            doc_background_color = val_colfnt;
+          };
+          i += 1;
+        };
+      };
+    };
+  };
+  std::fstream myfile(file);
   std::string cur_doc;
   std::vector<std::string> fun_tocv;
   std::vector<int> depth_tocv;
@@ -23,9 +138,9 @@ int main() {
   int cur_depth = 0;
   std::vector<std::string> scope_tocv;
   std::string cur_scope = "";
-  cur_doc = "<!DOCTYPE html>\n<head>\n<title>Documentation</title>\n";
+  cur_doc = "<!DOCTYPE html>\n<head>\n<title>" + title + "</title>\n";
   mvec.push_back(cur_doc);
-  cur_doc = "<style>table { font-family: arial, sans-serif; border-collapse: collapse; width: 50%; }\n td, th { border: 1px solid #dddddd; text-algn: left; padding: 8px; }\n code { font-family: Consolas, \"courrier new\"; color: #ac3a3a; padding: 2px; }\n div.Div { border: 1px solid black; background-color: #f1f1f1; text-align: left; width: 500px; margin-left: 20px; }\n hr.hr { border: dotted 2px; }\n div.Div2 { background-color: #2c4786; text-align: center; width: 100%; padding: 0px 0px; margin: 0; color: white; top: 0px; font-size: 25px; }\n </style>\n";
+  cur_doc = "<style>\ntable { border-collapse: collapse;\n width: 50%; }\n\ntd, th { border: 1px solid #dddddd;\n text-algn: left;\n padding: 8px; }\n\ncode { font-family: Consolas, \"courrier new\";\n color: " + code_color + ";\n padding: 2px; }\n\ndiv.Div { border: 1px solid black;\n background-color:" + code_background_color + ";\n text-align: left;\n width: 500px;\n margin-left: 20px;\n border-radius: 10px;\n padding: 5px; }\n\nhr.hr { border: dotted 2px; }\n\ndiv.Div2 { background-color:" + top_color + ";\n text-align: center;\n width: 100%;\n padding: 0px 0px;\n margin: 0;\n color: " + Title_color + ";\n top: 0px;\n font-size: 25px; }\n\ndiv.container { display: flex;\n gap: 25px;\n align-items: flex-start;\n box-sizing: border-box; }\n\ndiv.box1{ border: 2px solid black;\n background-color:" + toc_background_color + ";\n border-radius: 10px;\n text-align: left;\n width: 500px;\n position: sticky;\n top: 0;\n height: auto;\n overflow: visible;\n padding: 10px; }\n\ndiv.box2 { border: 2px solid black;\n background-color: " + doc_background_color + ";\n width: 100%;\n margin-right: 25px;\n border-radius: 15px;\n padding: 10px;\n }\nbody { background-color: " + background_color +";\nfont-family:" + font + ", sans-serif; }\np { color:" + font_color + "; }\n</style>\n";
   cur_doc += "</head>\n";
   cur_doc += "<body>\n";
   mvec.push_back(cur_doc);
@@ -36,8 +151,6 @@ int main() {
     logo_bool = 1;
   };
   std::string folder = " ";
-  unsigned int i;
-  unsigned int n;
   bool D = 0;
   bool A = 0;
   bool E = 0;
@@ -49,7 +162,7 @@ int main() {
       if (currow[2] == '@') {
         if (currow[3] == 'I') {
             if (!I) {
-              cur_doc = "<br><hr><h1><b id=\"INTRODUCTION\">INTRODUCTION:</b></h1>\n<p>";
+              cur_doc = "<br><hr><h1 style=\"color: " + titles_color + ";\"><b id=\"INTRODUCTION\">INTRODUCTION:</b></h1>\n<p>";
               fun_tocv.push_back("INTRODUCTION");
               bool_tocv.push_back(0);
               depth_tocv.push_back(-1);
@@ -150,7 +263,7 @@ int main() {
             };
             t_str = std::to_string(cur_depth);
             cur_doc = "<h" + t_str;
-            cur_doc += " style=\"color:#2c4786;\">";
+            cur_doc += " style=\"color:" + titles_color + ";\">";
             cur_scope = "";
             for (i = 6; i < n; ++i) {
               cur_scope += currow[i];
@@ -219,7 +332,7 @@ int main() {
             mvec.push_back(cur_doc);
         } else if (currow[3] == 'G') {
           t_str = currow[4];
-          cur_doc = "<h" + t_str + ">";
+          cur_doc = "<h" + t_str + " style=\"color: " + titles_color + ";\">";
           for (i = 6; i < n; ++i) {
             cur_doc += currow[i];
           };
@@ -229,7 +342,7 @@ int main() {
       };
     };
   };
-  mvec.push_back("</body>\n\n");
+  mvec.push_back("</div>\n</div>\n</body>\n\n");
   myfile.close();
   std::fstream outfile("out.html", std::ios::out);
   const unsigned int sizen = mvec.size();
@@ -241,16 +354,16 @@ int main() {
         outfile << "<div class=\"Div2\"><i><b>" + title + "</b></i></div><br>";
       };
       if (i == 3) {
-        outfile << "<div class=\"Div\"><a><i>Table Of Contents</i></a><br><br><ul>\n";
+        outfile << "<div class=\"container\">\n<div class=\"box1\"><a><i>Table Of Contents</i></a><br><br><ul>\n";
         for (cnt = 0; cnt < n2; ++cnt) {
           if (bool_tocv[cnt] != 0) {
-            outfile << "<b><li style=\"margin-left:" + std::to_string(20 * depth_tocv[cnt]) + "px; color: #2c4786;\">" + scope_tocv[cnt] + "</li></b>\n";
+            outfile << "<b><li style=\"margin-left:" + std::to_string(20 * depth_tocv[cnt]) + "px; color: " + titles_color + ";\">" + scope_tocv[cnt] + "</li></b>\n";
           };
           if (fun_tocv[cnt] != "X") {
             outfile << "<a href=\"#" + fun_tocv[cnt] + "\" style=\"margin-left:" + std::to_string(20 + 20 * depth_tocv[cnt]) + "px;\">" + fun_tocv[cnt] + "</a>\n<br>\n";
           };
         };
-        outfile << "</ul><br></div>\n";
+        outfile << "</ul><br></div>\n<div class=\"box2\">\n";
       };
       outfile << mvec[i];
     };
@@ -258,16 +371,16 @@ int main() {
       for (i = 0; i < sizen; ++i) {
         if (i == 2) {
           outfile << "<div class=\"Div2\"><i><b>" + title + "</b></i></div><br>";
-          outfile << "<div class=\"Div\"><a><i>Table Of Contents</i></a><br><br><ul>\n";
+          outfile << "<div class=\"container\">\n<div class=\"box1\"><a><i>Table Of Contents</i></a><br><br><ul>\n";
           for (cnt = 0; cnt < n2; ++cnt) {
             if (bool_tocv[cnt] != 0) {
-              outfile << "<b><li style=\"margin-left:" + std::to_string(20 * depth_tocv[cnt]) + "px; color: #2c4786;\">" + scope_tocv[cnt] + "</li></b>\n";
+              outfile << "<b><li style=\"margin-left:" + std::to_string(20 * depth_tocv[cnt]) + "px; color:" + titles_color + ";\">" + scope_tocv[cnt] + "</li></b>\n";
             };
             if (fun_tocv[cnt] != "X") {
               outfile << "<a href=\"#" + fun_tocv[cnt] + "\" style=\"margin-left:" + std::to_string(20 + 20 * depth_tocv[cnt]) + "px;\">" + fun_tocv[cnt] + "</a>\n<br>\n";
             };
           };
-          outfile << "</ul><br></div>\n";
+          outfile << "</ul><br></div>\n<div class=\"box2\">\n";
         };
         outfile << mvec[i];
       };
